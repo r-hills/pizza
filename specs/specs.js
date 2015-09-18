3caec69bd2c4bb3288e121171f96a6f8f8ab90f2
Rick Hills <rh-jobs@outlook.com>
2015-09-18 10:46:06 -0700

    Add test for prototype.getPrice() for Pizza object with multiple toppings.

--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,14 @@
 describe('Pizza', function() {
 	var toppings = ["Cheese"]; 
 
-	it("creates a new Pizza object with the given specs", function() {
+	it("Tests creating a new Pizza object with the default toppings", function() {
 		var testPizza = new Pizza("Medium", toppings); 
 
 		expect(testPizza.pizzaSize).to.equal("Medium");
 		expect(testPizza.toppings).to.eql(toppings); 
 	});
 
-	it("tests adding topping to topping array", function() {
+	it("Tests adding topping to topping array", function() {
 		var testPizza = new Pizza("Small", toppings);
 		testPizza.addTopping("Pepperoni");
 
@@ -16,7 +16,7 @@ describe('Pizza', function() {
 		expect(testPizza.toppings[1]).to.equal("Pepperoni");
 	});
 
-	it("tests getting price for a simple pizza", function() {
+	it("Tests getting price for a simple pizza", function() {
 		var testPizza = new Pizza("Small", ["Cheese"]);
 		var pizzaPrice = testPizza.getPrice();
 		console.log(pizzaPrice);
@@ -24,6 +24,10 @@ describe('Pizza', function() {
 		expect(pizzaPrice).to.equal(5.50); 
 	});
 
+	it("Tests getting the price of a pizza with multiple toppings", function () {
+		var testPizza = new Pizza("Large",["Cheese","Pepperoni","Sausage","Ham","Onions","Mushrooms"]);
 
+		expect(testPizza.getPrice()).to.equal(24.00);
+	})
 
 });
